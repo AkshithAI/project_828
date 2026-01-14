2050e8a438b313115c9dec5bd827d7e9b4574589
M <akshithmarepally111@gmail.com>
2026-01-14 12:41:51 +0530

    update configs

--- src/scripts/configs.py
+++ src/scripts/configs.py
@@ -6,20 +6,20 @@ from dataclasses import dataclass
 class ModelConfig:
         # Model architecture
         vocab_size : int = tokenizer.vocab_size   
-        num_attn_heads : int = 8 #8
+        num_attn_heads : int = 8 
         num_key_value_heads : int = 4
-        hidden_dim : int = 256  #512 
+        hidden_dim : int = 512  
         intermediate_size : int = 768
         ffn_dropout : float = 0.0
         head_dim : float = hidden_dim // num_attn_heads 
-        num_hidden_layers : int = 6 #1
-        num_experts : int = 4 #4
-        num_experts_per_tok : int = 2 #2
+        num_hidden_layers : int = 6 
+        num_experts : int = 6
+        num_experts_per_tok : int = 2 
         update_param : float = 1e-3
         route_scale : float = 1.0
         base : int = 10000
         initial_context_len : int = 2048
-        max_context_len : int = 2048 #4096
+        max_context_len : int = 2048 
         ntk_alpha : float = 1.0
         ntk_beta : float = 32.0
         scaling_factor : float = 1.0
