9be2ed69a18dad4203e5cc2a48bb2e0ea31d2822
M <akshithmarepally111@gmail.com>
2026-04-07 18:57:46 +0530

    revert back

--- src/models/model_flash_attn.py
+++ src/models/model_flash_attn.py
@@ -256,9 +256,8 @@ class MoE(nn.Module):
         sorted_out = torch.zeros_like(sorted_x)
         for i, expert in enumerate(self.experts):
             start, end = expert_boundaries[i].item(), expert_boundaries[i + 1].item()
-            if start == end:
-                continue
-            sorted_out[start:end] = expert(sorted_x[start:end])
+            if start < end:
+                sorted_out[start:end] = expert(sorted_x[start:end])
 
         # Weighted scatter-add back to original token positions
         sorted_out = sorted_out * sorted_weights
